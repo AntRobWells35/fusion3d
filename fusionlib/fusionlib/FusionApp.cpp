620cda4d5ce28ab1c60f8abc4e8d785d2df602c0
Anthony Robert Wells <scopedynamo@gmail.com>
2018-08-21 16:36:02 +0100

    intiail code running.

--- fusionlib/fusionlib/FusionApp.cpp
+++ fusionlib/fusionlib/FusionApp.cpp
@@ -2,34 +2,114 @@
 #include "FusionApp.h"
 
 
-FusionApp::FusionApp()
+FusionApp::FusionApp() {
+
+}
+
+FusionApp::FusionApp(int winWidth,int winHeight,string title,bool fullScreen)
 {
 
-	glfwInit();
-
-	glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-	GLFWwindow* window = glfwCreateWindow(800, 600, "Vulkan window", nullptr, nullptr);
-
-	uint32_t extensionCount = 0;
-	vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
-
-	std::cout << extensionCount << " extensions supported" << std::endl;
-
-	glm::mat4 matrix;
-	glm::vec4 vec;
-	auto test = matrix * vec;
-
-	while (!glfwWindowShouldClose(window)) {
-		glfwPollEvents();
-	}
-
-	glfwDestroyWindow(window);
-
-	glfwTerminate();
-
+	WinWidth = winWidth;
+	WinHeight = winHeight;
+	WinTitle = WinTitle;
+	WinFull = fullScreen;
+	
 }
 
 
 FusionApp::~FusionApp()
 {
+
+	CleanUp();
+
+}
+
+void FusionApp::CreateInstance() {
+
+	VkApplicationInfo appInfo = {};
+	appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+	appInfo.pApplicationName = WinTitle.c_str();
+	appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+	appInfo.pEngineName = "Fusion3D";
+	appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+	appInfo.apiVersion = VK_API_VERSION_1_0;
+
+	VkInstanceCreateInfo createInfo = {};
+	createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+	createInfo.pApplicationInfo = &appInfo;
+
+	uint32_t glfwExtensionCount = 0;
+	const char** glfwExtensions;
+
+	glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+	createInfo.enabledExtensionCount = glfwExtensionCount;
+	createInfo.ppEnabledExtensionNames = glfwExtensions;
+
+	createInfo.enabledLayerCount = 0;
+
+	if (vkCreateInstance(&createInfo, nullptr, &vInstance) != VK_SUCCESS) {
+		throw std::runtime_error("failed to create instance!");
+	}
+
+	cout << "Vulkan Initialized properly." << endl;
+
+	uint32_t extensionCount = 0;
+	vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+
+	std::vector<VkExtensionProperties> extensions(extensionCount);
+
+	vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, extensions.data());
+
+	std::cout << "available extensions:" << std::endl;
+
+	for (const auto& extension : extensions) {
+		std::cout << "\t" << extension.extensionName << std::endl;
+	}
+
+
+}
+
+void FusionApp::InitVulkan() {
+
+
+	CreateInstance();
+
+
+}
+
+void FusionApp::Run() {
+
+	glfwInit();
+
+	glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+	glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+	Win = glfwCreateWindow(WinWidth, WinHeight, WinTitle.c_str() , nullptr, nullptr);
+
+
+	InitVulkan();
+
+
+	RunApp();
+
+
+}
+
+void FusionApp::RunApp()
+{
+
+	while (!glfwWindowShouldClose(Win)) {
+		glfwPollEvents();
+	}
+
+}
+
+void FusionApp::CleanUp() {
+
+	vkDestroyInstance(vInstance, nullptr);
+
+	glfwDestroyWindow(Win);
+
+	glfwTerminate();
+
 }
--- fusionlib/fusionlib/FusionApp.h
+++ fusionlib/fusionlib/FusionApp.h
@@ -1,8 +1,29 @@
 #pragma once
+
+#include "stdafx.h"
+
+//#include <string>
+
+
+//using namespace std;
+
 class FusionApp
 {
 public:
 	FusionApp();
+	FusionApp(int winWidth, int winHeight, string title, bool fullScreen);
 	virtual ~FusionApp();
+	void Run();
+private:
+	void CleanUp();
+	void InitVulkan();
+	void RunApp();
+	GLFWwindow * Win;
+	int WinWidth, WinHeight;
+	string WinTitle;
+	bool WinFull;
+	void CreateInstance();
+	VkInstance vInstance;
+	
 };
 
