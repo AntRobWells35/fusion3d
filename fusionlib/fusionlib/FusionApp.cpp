4b4a778b64d481c80e65b4b570821b361b452e07
Anthony Robert Wells <scopedynamo@gmail.com>
2018-08-21 16:55:53 +0100

    update

--- fusionlib/fusionlib/FusionApp.cpp
+++ fusionlib/fusionlib/FusionApp.cpp
@@ -2,6 +2,16 @@
 #include "FusionApp.h"
 
 
+const std::vector<const char*> validationLayers = {
+"VK_LAYER_LUNARG_standard_validation"
+};
+
+#ifdef NDEBUG
+const bool enableValidationLayers = false;
+#else
+const bool enableValidationLayers = true;
+#endif
+
 FusionApp::FusionApp() {
 
 }
@@ -24,7 +34,51 @@ FusionApp::~FusionApp()
 
 }
 
+
+bool checkValidationLayerSupport() {
+
+
+
+
+
+
+	uint32_t layerCount;
+	vkEnumerateInstanceLayerProperties(&layerCount, nullptr);
+
+	std::vector<VkLayerProperties> availableLayers(layerCount);
+	vkEnumerateInstanceLayerProperties(&layerCount, availableLayers.data());
+
+	for (const char* layerName : validationLayers) {
+		bool layerFound = false;
+
+		for (const auto& layerProperties : availableLayers) {
+			if (strcmp(layerName, layerProperties.layerName) == 0) {
+				layerFound = true;
+				break;
+			}
+		}
+
+		if (!layerFound) {
+			return false;
+		}
+	}
+
+	return true;
+}
+
+
 void FusionApp::CreateInstance() {
+
+
+#ifdef NDEBUG
+	const bool enableValidationLayers = false;
+#else
+	const bool enableValidationLayers = true;
+#endif
+
+	if (enableValidationLayers && !checkValidationLayerSupport()) {
+		throw std::runtime_error("validation layers requested, but not available!");
+	}
 
 	VkApplicationInfo appInfo = {};
 	appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
@@ -35,6 +89,14 @@ void FusionApp::CreateInstance() {
 	appInfo.apiVersion = VK_API_VERSION_1_0;
 
 	VkInstanceCreateInfo createInfo = {};
+
+	if (enableValidationLayers) {
+		createInfo.enabledLayerCount = static_cast<uint32_t>(validationLayers.size());
+		createInfo.ppEnabledLayerNames = validationLayers.data();
+	}
+	else {
+		createInfo.enabledLayerCount = 0;
+	}
 	createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
 	createInfo.pApplicationInfo = &appInfo;
 
@@ -85,7 +147,6 @@ void FusionApp::Run() {
 	glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
 	glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
 	Win = glfwCreateWindow(WinWidth, WinHeight, WinTitle.c_str() , nullptr, nullptr);
-
 
 	InitVulkan();
 
